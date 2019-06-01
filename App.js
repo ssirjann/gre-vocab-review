f016950a7d2d91e788abab7e866c8b018b6a94f0
Sirjan <sharsir.ss@gmail.com>
2019-06-01 20:42:06 +0545

    Init with basic header

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+      title: "Home"
+    };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text onPress={() => this.props.navigation.push("Details")}>
+          Home Screen
+        </Text>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+static navigationOptions = {
+    title: "Details"
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home",
+    headerLayoutPreset: "center",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5388d0"
+      },
+      headerTitleStyle: {
+        letterSpacing: 2,
+        fontWeight: "normal"
+      },
+      headerRight: <View />,
+      headerTintColor: "#ffffff"
+    }
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
